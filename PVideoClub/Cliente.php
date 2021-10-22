3d912f87afd674e9453b51e44617ed16aade44ba
antoniohernandezruz2002@gmail.com <antoniohernandezruz2002@gmail.com>
2021-10-22 16:07:05 +0200

    Clases cliente  medio terminar

--- PVideoClub/Cliente.php
+++ PVideoClub/Cliente.php
@@ -1,14 +1,35 @@
 <?php
+declare(strict_types=1);
+include_once("Soporte.php");
 class Cliente{
-
+        private array $soporteAlquilado=[];
+        private int $numSoporteAlquilado;
+        
     public function __construct(
         public string $nombre,
-        private int $numero,
-        private array $soporteAlquilado,
-        private int $numSoporteAlquilado,
+        private int $numero, 
         private int $maxAlquilerConcurente=3
-    ){
-        $this->soporteAlquilado=[];
+    ){}
+    public function getNumero(){
+        return $this->numero;
+    }
+    public function setNumero(int $numero){
+        $this->numero = $numero;
+    }
+    public function getNumSoporteAlquilado(){
+        return $this->numSoporteAlquilado;
+    }
+    public function mostrarResumen(){
+        echo "<br>Nombre: ". $this->nombre. "<br> Cantidad de Alquileres: ". count($this->soporteAlquilado);
+    }
+    public function tieneAlquilado(Soporte $s): bool{
+        $resultado = false;
+        foreach($this->soporteAlquilado as $soporte){
+            if ($soporte instanceof $s){
+                $resultado=true;
+            }
+        }
+        return $resultado;
     }
 }
 ?>
